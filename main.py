6e1269317a28fa83dd94cd8ee0c4d13f5278f4c7
daniel.wiszniowski <mrcabbagestick@gmail.com>
2025-02-23 18:41:28 +0100

    Added basic bot template

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-#For token importing
+# For extracting TOKEN from the environment
 from dotenv import load_dotenv
 import os
 
-#The main library for using discord API
+# For pseudorandom number generation
+import random
+
+# The main library for using discord API
 import discord
 
 
-load_dotenv() # load all the variables from the env file
+load_dotenv() # Load all the variables from the env file
 bot = discord.Bot()
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
 
-bot.run(os.getenv('TOKEN')) #Runs the bot with the token
 
-#Repo: https://github.com/skni-umcs/pycord-tutorial
+bot.run(os.getenv('TOKEN')) # Runs the bot with the token
+
+# Repo: https://github.com/skni-umcs/pycord-tutorial
